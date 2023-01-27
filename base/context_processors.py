c10a4c450a49200ff43b252bfb10eea7485d220e
Hostine <austinjuniour4@gmail.com>
2023-01-27 14:39:15 +0300

    bugs

--- base/context_processors.py
+++
@@ -1,8 +0,0 @@
-from .models import Appointment
-
-def get_notification(request):
-    count = Appointment.objects.filter(accepted=False).count()
-    data = {
-        "count":count
-    }
-    return data

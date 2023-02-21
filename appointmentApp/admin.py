a3e7946ce314e8f20b956fb64a40d7df236c3962
Hostine <austinjuniour4@gmail.com>
2023-02-21 13:02:24 +0300

    add db VC

--- appointmentApp/admin.py
+++ appointmentApp/admin.py
@@ -1,9 +1,19 @@
 
 # Register your models here.
 from django.contrib import admin
-from .models import Appointment
+from .models import Appointment, VirtualCounsellor
 
 # Register your models here.
 
 # appointment register
 admin.site.register(Appointment)
+
+
+class vcForm(admin.ModelAdmin):
+    list_display = ('name',
+                    'mail',
+                    'phone_number',
+                    'date_time')
+
+
+admin.site.register(VirtualCounsellor, vcForm)

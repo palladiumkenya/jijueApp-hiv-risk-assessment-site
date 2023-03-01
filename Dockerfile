ace414f71c94745825692eddbafeae9937eee4fb
murithijoshua <brodiamyzalius@gmail.com>
2023-03-01 23:09:00 +0300

    (improvements) docker updates

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,8 @@
-FROM python:3.11-slim-buster
-# ENV PYTHONUNBUFFERED=1
-
-WORKDIR /jijueapp
-
-COPY requirements.txt ./
-
-RUN pip3 install --no-cache-dir -r requirements.txt
-
-COPY . .
-
+FROM python:3.8
+ENV PYTHONUNBUFFERED 1
+WORKDIR /app
+COPY . /app
 EXPOSE 8000
-CMD ["python3", "manage.py", "runserver", "127.0.0.1:8000"]
+RUN pip install -r requirements.txt
+RUN python manage.py migrate
+CMD [ "python", "manage.py","runserver","0.0.0.0:8000" ]
--- jijueApp/settings.py
+++ jijueApp/settings.py
@@ -71,10 +71,34 @@ WSGI_APPLICATION = 'jijueApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'JIJUE',
+#         'USER': 'jijue',
+#         'PASSWORD': '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',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
+"""
+For development 
+"""
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'JIJUE',
+        'USER': 'juser',
+        'PASSWORD': 'juser',
+        'HOST': 'testing_database',
+        'PORT': '3306',
     }
 }
 

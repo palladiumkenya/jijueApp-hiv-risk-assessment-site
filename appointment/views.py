95803879678bfe90d099d1ee3dff41b923a5ce43
sphinx-austin <austinjuniour4@gmail.com>
2023-03-28 12:37:29 +0300

    load facilities(appointment)

--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from . models import County, SubCounty, Ward, Facility
 
 # Create your views here.
+
+
+def counties(request):
+    counties = County.objects.all()
+    context = {'counties': counties}
+    return render(request, 'appointment/appoint_ment.html', context)
+
+
+def sub_counties(request):
+    county = request.GET.get('county')
+    sub_counties = SubCounty.objects.filter(county=county)
+    context = {'sub_counties': sub_counties, 'is_htmx': True}
+    return render(request, 'partials/modules.html', context)
+
+
+def wards(request):
+    sub_county = request.GET.get('sub_county')
+    wards = Ward.objects.filter(sub_county=sub_county)
+    context = {'wards': wards}
+    return render(request, 'partials/ward.html', context)
